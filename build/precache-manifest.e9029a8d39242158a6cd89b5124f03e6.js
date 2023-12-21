cae6ca71bff7f34801937335e372df09c8943fdf
Mohana Sravya <79496726+Mohana810@users.noreply.github.com>
2023-12-21 20:42:55 +0530

    update

---
+++ build/precache-manifest.e9029a8d39242158a6cd89b5124f03e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f4c4ddddadab9242f7e1d2d304b6b75",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a27bfbeea33a609ac844",
+    "url": "/static/css/2.fd277b09.chunk.css"
+  },
+  {
+    "revision": "e13cb003d1d71b9dcb1b",
+    "url": "/static/css/main.c901d1eb.chunk.css"
+  },
+  {
+    "revision": "a27bfbeea33a609ac844",
+    "url": "/static/js/2.e8378970.chunk.js"
+  },
+  {
+    "revision": "46a379a572333860f78765c743603e43",
+    "url": "/static/js/2.e8378970.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e13cb003d1d71b9dcb1b",
+    "url": "/static/js/main.33c2067f.chunk.js"
+  },
+  {
+    "revision": "430f72daf7c3a4d42642",
+    "url": "/static/js/runtime-main.351e9c47.js"
+  }
+]);
